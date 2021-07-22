f3b994ac2c042c9e647a6650aac27f085143913b
Vitor Silva <vitor.o.silva95@gmail.com>
2021-07-21 21:36:45 -0300

    Requisito 14

--- script.js
+++ script.js
@@ -10,6 +10,23 @@ function checkLoginAndPassword() {
     alert('Login ou senha inválidos.');
   }
 }
+const evaluateTrybe = document.getElementById('evaluate-trybe-radio');
 
+for (let index = 1; index < 11; index += 1) {
+  const div = document.createElement('div');
+  div.className = 'form-check';
+  evaluateTrybe.appendChild(div);
+
+  const label = document.createElement('label');
+  label.innerText = index;
+  div.appendChild(label);
+  label.className = 'form-check-label';
+
+  const inputRadio = document.createElement('input');
+  inputRadio.setAttribute('type', 'radio');
+  inputRadio.setAttribute('value', index);
+  inputRadio.setAttribute('name', 'rate');
+  div.appendChild(inputRadio);
+}
 const loginButton = document.getElementById('login-button');
 loginButton.addEventListener('click', checkLoginAndPassword);
