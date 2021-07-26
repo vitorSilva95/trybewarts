df778583e3ff27ee81c7318f52f5f1ddfa9f9200
Bruno M Landim <brunolandim13@hotmail.com>
2021-07-26 14:10:22 -0300

    Adicionando teste de requisito 21

--- script.js
+++ script.js
@@ -31,7 +31,9 @@ for (let index = 1; index < 11; index += 1) {
 
 const counterTextArea = document.getElementById('counter');
 const textArea = document.querySelector('#textarea');
+
 textArea.addEventListener('keyup', () => {
+
   const inputLength = textArea.value.length;
   counterTextArea.innerText = 500 - (inputLength);
   console.log(counterTextArea);
@@ -43,7 +45,37 @@ loginButton.addEventListener('click', checkLoginAndPassword);
 const mandatorybutton = document.querySelector('#agreement');
 const enablebutton = document.querySelector('#submit-btn');
 
-function checkSelectedButton() {
-  enablebutton.disabled = false;
-}
-mandatorybutton.addEventListener('click', checkSelectedButton);
+//Evento de click no checkbox / Habilita botão enviar se checkbox estiver marcado
+mandatorybutton.addEventListener('click', () => {
+  enablebutton.disabled === true ? enablebutton.disabled = false : enablebutton.disabled = true;
+});
+const main = document.querySelector('#main');
+const formText = document.querySelector('#evaluation-form');
+
+const inputname = document.querySelector('#input-name');
+const inputlastname = document.querySelector('#input-lastname');
+const select = document.querySelector('#house');
+const email = document.querySelector('#input-email');
+const checkbox = document.querySelector('#checkboxtext');
+const itemradio = document.querySelector('input[name="family"]:checked').value;
+console.log(itemradio);
+
+/* let radiochecked = '';
+for (let index = 0; index < radio.length; index += 1) {
+  if(radio[index].checked === true) {
+    radiochecked = radio[index].value;
+  }
+} */
+
+
+enablebutton.addEventListener('click', (event) => {
+  event.preventDefault();
+  formText.innerHTML = '';
+  const newP = document.createElement('p');
+  newP.innerHTML = `Familia ${itemradio}`
+
+
+formText.appendChild(newP)
+
+});
+console.log();
